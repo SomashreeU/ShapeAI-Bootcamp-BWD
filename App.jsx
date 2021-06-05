0832cb2b1cb10639b6d703c5488162bbb9579e2d
SomashreeU <82863411+SomashreeU@users.noreply.github.com>
2021-06-05 11:49:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and Reactjs Bootcamp</h1>
+      <p>
+        This was an amazing bootcamp taken up Sharuya Sinha . We have covered
+        everything from scratch including Javascript , Reactjs and HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
